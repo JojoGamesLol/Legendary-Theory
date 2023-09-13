3556b52dbdb7bc4cd193c327dd51e48b70f2b236
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-12 21:33:15 -0300

    Add files via upload

--- Theory1.js
+++ Theory1.js
@@ -11,9 +11,24 @@ var authors = "JojoGames320";
 var version = "1.0.0";
 
 var currency;
+var chapter1
 
 var init = () => {
 	currency = theory.createCurrency();
+	
+	// Regular Upgrades
+	//chapter1
+	{
+        let getDesc = () => "Chapter 1";
+        let getInfo = () => "Open a popup with chapter 1";
+        chpater1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(800, 1.44)));
+        chapter1.getDescription = (_) => getDesc();
+        chapter1.getInfo = () => getInfo();
+        chapter1.bought = (amount) = {
+        	chapter1.level -= amount
+        	chapter1Popup.show();
+        }
+    }
 }
 
 init();
